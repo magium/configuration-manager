78369913146d36b798c54b820a61b1eebd2cef14
Kevin Schroeder <kschroeder@mirageworks.com>
2017-02-16 15:37:29 -0600

    Added configuration set

--- lib/Console/Symfony/CommandList.php
+++ lib/Console/Symfony/CommandList.php
@@ -5,6 +5,7 @@ namespace Magium\Configuration\Console\Symfony;
 use Magium\Configuration\Console\Command\ConfigurationBuild;
 use Magium\Configuration\Console\Command\ConfigurationGet;
 use Magium\Configuration\Console\Command\ConfigurationList;
+use Magium\Configuration\Console\Command\ConfigurationSet;
 use Magium\Configuration\Console\Command\ContextList;
 use Magium\Configuration\Console\Command\CreateTable;
 use Magium\Configuration\Console\Command\DefaultCommand;
@@ -18,6 +19,7 @@ class CommandList
         $application->add(new DefaultCommand());
         $application->add(new ConfigurationBuild());
         $application->add(new ConfigurationGet());
+        $application->add(new ConfigurationSet());
         $application->add(new ConfigurationList());
         $application->add(new ContextList());
         $application->add(new CreateTable());
