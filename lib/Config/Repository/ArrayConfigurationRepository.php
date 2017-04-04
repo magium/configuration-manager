31f899d985d07b9cd1c57b5d9bc92c6beb688265
Kevin Schroeder <kschroeder@mirageworks.com>
2017-04-04 12:59:14 -0500

    Added static array configuration object

---
+++ lib/Config/Repository/ArrayConfigurationRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Magium\Configuration\Config\Repository;
+
+use Magium\Configuration\Config\InvalidArgumentException;
+
+class ArrayConfigurationRepository implements ConfigInterface
+{
+
+    protected $data;
+
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    public function getValue($path)
+    {
+        return $this->xpath($path);
+    }
+
+    public function hasValue($path)
+    {
+        return $this->xpath($path) !== null;
+    }
+
+    public function getValueFlag($path)
+    {
+        $value = $this->getValue($path);
+        return in_array($value, self::ALLOWED_TRUES);
+    }
+
+    public function xpath($xpath)
+    {
+        $parts = explode('/', $xpath);
+        if (count($parts) != 3) {
+            throw new InvalidArgumentException('Path must be in the form of "section/group/element"');
+        }
+        $process = $this->data;
+        foreach ($parts as $part) {
+            if (isset($process[$part])) {
+                $process = $process[$part];
+            } else {
+                return null;
+            }
+        }
+        return $process;
+    }
+
+
+}
