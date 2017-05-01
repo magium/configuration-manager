f62547fdbf68aa52edf1df995c33cc5d79305f0b
Kevin Schroeder <kschroeder@mirageworks.com>
2017-05-01 11:19:50 -0500

    Read functionality for the frontend working

---
+++ lib/View/Controllers/Refresh.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Magium\Configuration\View\Controllers;
+
+use Interop\Container\ContainerInterface;
+use Magium\Configuration\Config\BuilderInterface;
+use Magium\Configuration\Config\Repository\ConfigInterface;
+use Magium\Configuration\Config\MergedStructure;
+use Magium\Configuration\View\ViewConfiguration;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Refresh implements ControllerInterface
+{
+
+    protected $viewConfiguration;
+    protected $builder;
+    protected $mergedConfiguration;
+    protected $config;
+    protected $container;
+
+    public function __construct(
+        ViewConfiguration $viewConfiguration,
+        BuilderInterface $builder,
+        MergedStructure $mergedConfiguration,
+        ConfigInterface $config,
+        ContainerInterface $container = null
+    )
+    {
+        $this->viewConfiguration = $viewConfiguration;
+        $this->config = $config;
+        $this->mergedConfiguration = $mergedConfiguration;
+        $this->builder = $builder;
+        $this->container = $container;
+    }
+
+    public function execute(ServerRequestInterface $request)
+    {
+        
+    }
+
+}
